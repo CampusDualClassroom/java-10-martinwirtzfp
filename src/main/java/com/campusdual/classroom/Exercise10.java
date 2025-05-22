0596dd104ace1bc2a59b5049847ccedcac550b98
Martin Sanchez Novo <martinwirtzfp@gmail.com>
2025-05-22 13:30:26 +0200

    Ejercicio 10 completado y testeado

--- src/main/java/com/campusdual/classroom/Exercise10.java
+++ src/main/java/com/campusdual/classroom/Exercise10.java
@@ -9,18 +9,36 @@ public class Exercise10 {
     // El mensaje a imprimir es → La bola es de color: COLOR_BOLA
     // COLOR_BOLA puede ser → rojo, azul, verde
     public static void main(String[] args) {
-
+        int contador = 0;
+        String color_bola;
+        do {
+            color_bola = getBall();
+            System.out.println("La bola es de color: " + color_bola);
+            if (color_bola.equals("azul")){
+                contador++;
+            }
+        } while(contador < 2);
     }
 
     //TODO ↓
-    // Un método que devuelva una cadena de texto con el color de una pelota (en minúscula). Dicho color se escogerá de
-    // manera aleatoria. Se empleará el método randomWithRange(int min, int max) para escoger un número aleatorio, que
+    // Un métod que devuelva una cadena de texto con el color de una pelota (en minúscula). Dicho color se escogerá de
+    // manera aleatoria. Se empleará el métod randomWithRange(int min, int max) para escoger un número aleatorio, que
     // lo asociaremos al valor de un color de la siguiente manera:
     // 1 → rojo
     // 2 → azul
     // 3 → verde
     public static String getBall() {
-        return null;
+        int aleatorio = randomWithRange(1,4);
+        String color = null;
+        switch (aleatorio){
+            case 1: {color = "rojo";
+                break;}
+            case 2: {color = "azul";
+            break;}
+            case 3: {color = "verde";
+            break;}
+        }
+        return color;
     }
 
     public static int randomWithRange(int min, int max) {
